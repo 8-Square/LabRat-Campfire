a5b02dcd911ccc2a533c24f42f99de9278563736
4_Square <104998118+8-Square@users.noreply.github.com>
2026-03-01 08:47:46 +1000

    Not exactly working...

--- assets/Scripts/player.gd
+++ assets/Scripts/player.gd
@@ -4,8 +4,10 @@ class_name Player extends CharacterBody2D
 @export var respawn_point: Node2D
 
 @onready var stage_bit: StageBit = $StageBit
-#@onready var timer: Timer = $Timer
 @onready var animated_sprite: AnimatedSprite2D
+
+# Tilemap, for death loop
+@onready var tile_map_layer: TileMapLayer = $"../TileMapLayer"
 
 const SPEED = 250
 const SPRINT_SPEED = 400
@@ -16,7 +18,7 @@ var toggled_sprint: bool = false
 var is_walking: bool = false
 
 var can_control: bool = true
-
+var death_count: float = 0
 
 @export var allowed_direction := Vector2.RIGHT
 
@@ -65,8 +67,8 @@ func _physics_process(delta: float) -> void:
 		is_walking = true
 		
 		# One way wall
-		if is_touching_one_way_wall():
-			pass_through_wall()
+		#if is_touching_one_way_wall():
+			#pass_through_wall()
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		animated_sprite.play("idle")
@@ -79,40 +81,60 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("reset"):
 		reset()
 	
-	
+	check_spikes()
 	move_and_slide()
 
 
 func reset() -> void:
 	can_control = false
 	velocity = Vector2.ZERO
-	await get_tree().create_timer(0.2).timeout
+	await get_tree().create_timer(0.4).timeout
 	global_position = respawn_point.global_position
 	can_control = true
 
 # Spikes/Death Loop
 func check_spikes():
-	pass
-
-
-
-func is_touching_one_way_wall() -> bool:
+	# Gets cells (Squares) in tilemap 
+	var cell: Vector2i = tile_map_layer.local_to_map(tile_map_layer.to_local(global_position))
+	# Gets data of current cell (square) of tilemap 
+	var tile_data: TileData = tile_map_layer.get_cell_tile_data(cell)
+	# checks for data and if it has damage_block (for deaht of player)
+	#if tile_data:
+		#if (tile_data.get_custom_data("DamageTile") or tile_data.get_custom_data("damage_block")) and can_control:
+			#print("PLAYER DIED")
+			#death_count += 1
+			#reset()
+	
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
-		if collision.get_collider() && (1 << (one_way_layer - 1)):
-			return true
-	return false
+		var collider = collision.get_collider()
+		if collider == tile_map_layer:
+			# Convert collision point to tile coordinates
+			var tile_pos = tile_map_layer.local_to_map(collision.get_position())     
+			# Check if there's a tile at this position
+			if tile_map_layer.get_cell_source_id(tile_pos) != -1:
+				if tile_map_layer.get_cell_tile_data(tile_pos):
+					if tile_data.get_custom_data("damage_block") and can_control:
+					#reset()
+						print("HITTING THE RIGHT ONE..?")
 
 
-func pass_through_wall():
-	passing_through = true
-	
-	# Disable collision with one-way wall layer
-	set_collision_mask_value(one_way_layer, false)
-
-	await get_tree().create_timer(pass_through_time).timeout
-
-	# Re-enable collision
-	set_collision_mask_value(one_way_layer, true)
-	
-	passing_through = false
+# RANDOM, PROBABLY GONNA REMOVE
+#func is_touching_one_way_wall() -> bool:
+	#for i in get_slide_collision_count():
+		#var collision = get_slide_collision(i)
+		#if collision.get_collider() && (1 << (one_way_layer - 1)):
+			#return true
+	#return false
+#func pass_through_wall():
+	#passing_through = true
+	#
+	## Disable collision with one-way wall layer
+	#set_collision_mask_value(one_way_layer, false)
+#
+	#await get_tree().create_timer(pass_through_time).timeout
+#
+	## Re-enable collision
+	#set_collision_mask_value(one_way_layer, true)
+	#
+	#passing_through = false
