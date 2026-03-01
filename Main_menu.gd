3f9c7abd661e3995352cb6a573917e184e1c5a0b
4_Square <104998118+8-Square@users.noreply.github.com>
2026-03-01 11:26:10 +1000

    Stay

--- Main_menu.gd
+++ Main_menu.gd
@@ -1,21 +1,19 @@
-extends Node2D
+extends Control
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
 
+
 func _on_play_button_pressed():
-	AnimationPlayer
-	get_tree().change_scene_to_file("res://assets/Scenes/game.tscn")
+	animation_player.play("start")
+	await animation_player.animation_finished
+	print("Moving to game")
+	get_tree().change_scene_to_file("res://assets/Scenes/Sewers/Game.tscn")
+
+
 
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
-	
-	
